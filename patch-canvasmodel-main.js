e538c550bb68f2e3ed838a971ed36897c75d22f4
suin14 <sil0410@163.com>
2023-10-12 10:56:19 +0800

    整合

--- patch-canvasmodel-main.js
+++ patch-canvasmodel-main.js
@@ -13,106 +13,106 @@
          *
          *
          */
-        // "fenghuang_pendant": {
-        //     srcfn(options) {
-        //         return `img/transformations/fenghuang/pendant/${options.fenghuang_pendant_type}.png`;
-        //     },
-        //     showfn(options) {
-        //         return options.show_tf && isPartEnabled(options.fenghuang_pendant_type);
-        //     },
-        //     z: ZIndices.over_upper,
-        //     animation: "idle"
-        // },
-        "fenghuang_crest": {
-            srcfn(options) {
-                return `img/transformations/fenghuang/crest/${options.fenghuang_crest_type}.png`;
-            },
-            showfn(options) {
-                return options.show_tf && isPartEnabled(options.fenghuang_crest_type);
-            },
-            z: ZIndices.backhair,
-            animation: "idle"
-        },
-        "fenghuang_crestover": {
-            srcfn(options) {
-                return `img/transformations/fenghuang/crestover/${options.fenghuang_crest_type}.png`;
-            },
-            showfn(options) {
-                return options.show_tf && isPartEnabled(options.fenghuang_crest_type);
-            },
-            z: ZIndices.over_upper,
-            animation: "idle"
-        },
-        "fenghuang_eyes": {
-            srcfn(options) {
-                return `img/transformations/fenghuang/eyes/${options.fenghuang_eyes_type}.png`;
-            },
-            showfn(options) {
-                return options.show_tf && options.show_face && isPartEnabled(options.fenghuang_eyes_type)
-            },
-            z: ZIndices.irisacc,
-            animation: "idle"
-        },
-        "fenghuang_wings_right": {
-            srcfn(options) {
-                return `img/transformations/fenghuang/rightwing/${options.fenghuang_wings_type}.png`;
-            },
-            showfn(options) {
-                return options.show_tf && isPartEnabled(options.fenghuang_wings_type)
-            },
-            z: ZIndices.backhair,
-            animation: "idle"
-        },
-        "fenghuang_wings_left": {
-            srcfn(options) {
-                return `img/transformations/fenghuang/leftwing/${options.fenghuang_wings_type}.png`;
-            },
-            showfn(options) {
-                return options.show_tf && isPartEnabled(options.fenghuang_wings_type)
-            },
-            z: ZIndices.backhair,
-            animation: "idle"
-        },
-        "fenghuang_wings_rightover": {
-            srcfn(options) {
-                return `img/transformations/fenghuang/rightover/${options.fenghuang_wings_type}.png`;
-            },
-            showfn(options) {
-                return options.show_tf && isPartEnabled(options.fenghuang_wings_type)
-            },
-            z: ZIndices.over_upper,
-            animation: "idle"
-        },
-        "fenghuang_wings_leftover": {
-            srcfn(options) {
-                return `img/transformations/fenghuang/leftover/${options.fenghuang_wings_type}.png`;
-            },
-            showfn(options) {
-                return options.show_tf && isPartEnabled(options.fenghuang_wings_type)
-            },
-            z: ZIndices.over_upper,
-            animation: "idle"
-        },
-        "fenghuang_tail": {
-            srcfn(options) {
-                return `img/transformations/fenghuang/tail/${options.fenghuang_tail_type}.png`;
-            },
-            showfn(options) {
-                return options.show_tf && isPartEnabled(options.fenghuang_tail_type)
-            },
-            z: ZIndices.backhair,
-            animation: "idle"
-        },
-        "fenghuang_tattoo": {
-            srcfn(options) {
-                return `img/transformations/fenghuang/tattoo/${options.fenghuang_tattoo_type}.png`;
-            },
-            showfn(options) {
-                return options.show_tf && isPartEnabled(options.fenghuang_tattoo_type);
-            },
-            z: ZIndices.lower,
-            animation: "idle"
-        },
+        "fenghuang_pendant": {
+			srcfn(options) {
+				return `img/transformations/${options.closeup}/fenghuang/pendant/${options.fenghuang_pendant_type}.png`;
+			},
+			showfn(options) {
+				return options.show_tf && isPartEnabled(options.fenghuang_pendant_type);
+			},
+			z: ZIndices.lower_top,
+			animation: "idle"
+		},
+		 "fenghuang_crest": {
+			srcfn(options) {
+				return `img/transformations/${options.closeup}/fenghuang/crest/${options.fenghuang_crest_type}.png`;
+			},
+			showfn(options) {
+				return options.show_tf && isPartEnabled(options.fenghuang_crest_type);
+			},
+			z: ZIndices.backhair,
+			animation: "idle"
+		},
+		"fenghuang_crestover": {
+			srcfn(options) {
+				return `img/transformations/${options.closeup}/fenghuang/crestover/${options.fenghuang_crest_type}.png`;
+			},
+			showfn(options) {
+				return options.show_tf && isPartEnabled(options.fenghuang_crest_type);
+			},
+			z: ZIndices.over_upper,
+			animation: "idle"
+		},
+		"fenghuang_eyes": {
+			srcfn(options) {
+				return `img/transformations/${options.closeup}/fenghuang/eyes/${options.fenghuang_eyes_type}.png`;
+			},
+			showfn(options) {
+				return options.show_tf && options.show_face && isPartEnabled(options.fenghuang_eyes_type)
+			},
+			z: ZIndices.irisacc,
+			animation: "idle"
+		},
+		"fenghuang_wings_right": {
+			srcfn(options) {
+				return `img/transformations/${options.closeup}/fenghuang/rightwing/${options.fenghuang_wings_type}.png`;
+			},
+			showfn(options) {
+				return options.show_tf && isPartEnabled(options.fenghuang_wings_type) 
+			},
+			z: ZIndices.backhair,
+			animation: "idle"
+		},
+		"fenghuang_wings_left": {
+			srcfn(options) {
+				return `img/transformations/${options.closeup}/fenghuang/leftwing/${options.fenghuang_wings_type}.png`;
+			},
+			showfn(options) {
+				return options.show_tf && isPartEnabled(options.fenghuang_wings_type)
+			},
+			z: ZIndices.backhair,
+			animation: "idle"
+		},
+		"fenghuang_wings_rightover": {
+			srcfn(options) {
+				return `img/transformations/${options.closeup}/fenghuang/rightover/${options.fenghuang_wings_type}.png`;
+			},
+			showfn(options) {
+				return options.show_tf && isPartEnabled(options.fenghuang_wings_type) 
+			},
+			z: ZIndices.over_upper,
+			animation: "idle"
+		},
+		"fenghuang_wings_leftover": {
+			srcfn(options) {
+				return `img/transformations/${options.closeup}/fenghuang/leftover/${options.fenghuang_wings_type}.png`;
+			},
+			showfn(options) {
+				return options.show_tf && isPartEnabled(options.fenghuang_wings_type)
+			},
+			z: ZIndices.over_upper,
+			animation: "idle"
+		},
+		"fenghuang_tail": {
+			srcfn(options) {
+				return `img/transformations/${options.closeup}/fenghuang/tail/${options.fenghuang_tail_type}.png`;
+			},
+			showfn(options) {
+				return options.show_tf && isPartEnabled(options.fenghuang_tail_type)
+			},
+			z: ZIndices.backhair,
+			animation: "idle"
+		},
+		"fenghuang_tattoo": {
+			srcfn(options) {
+				return `img/transformations/${options.closeup}/fenghuang/tattoo/${options.fenghuang_tattoo_type}.png`;
+			},
+			showfn(options) {
+				return options.show_tf && isPartEnabled(options.fenghuang_tattoo_type);
+			},
+			z: ZIndices.lower,
+			animation: "idle"
+		},
     };
 
     // canvasmodel-main.js
